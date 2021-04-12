5bd448749d1ef34999d0ca864638da76cdb7f5b9
Angelines Yaport-Garcia <angelinesyaport-garcia@Angeliness-MBP.fios-router.home>
2021-04-12 15:21:35 -0400

    Cat_Facts

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 $(() => { //on load begins
 
-  let currentImgIndex = 0;
-  let lastIndex = $('.carousel-images').children().length-1;//+data
+  let currentImgIndex = 0;//sets current image @ indx 0 which selects 1st image
+  let lastIndex = $('.carousel-images').children().length-1;//takes array minus one because index starts at 0 so it will take off the empy spot at the end.
   // let $userInput = $('input[type="click"]').val()
   // let $getDataFact = $(event.target).val()
-  $('button').on('click', (event)=> {
-    // event.target.preventDefault()
+  $('button').on('click', (event)=> { //sets what will happen on click of bttn
+    // event.target.preventDefault()   //which is the event listener
 
     $.ajax({
-      url:'https://cat-fact.herokuapp.com/facts',
+      url:'https://cat-fact.herokuapp.com/facts',//url/api pulling from
 
     }).then( //make carousel here and append to it
       (data)=>{
-        $('.fact').remove()
-        $(".carousel-images")
-        .children()
+        $('.fact').remove()//removes last fact and puts new one when clicked
+        $(".carousel-images")//takes the images in the carousel
+        .children()          // and hides them until called upon by the click
         .eq(currentImgIndex)
-        .css("display","none");
+        .css("display","none");//this hides them
         //
-        let $userInput = $('input[type="click"]').val()
+        let $userInput = $('input[type="click"]').val()//establishes what the interacton from the user will do and refers back to event listener that says that the interaction will be a click.
         // let $data = $(event.target).val('<div>')
-        const $data = $('<div>')
+        const $data = $('<div>')//sets the data from the API as a div in order to appear on the page.
 
           if (currentImgIndex < lastIndex) {
-            currentImgIndex++;
-            }else {
-            currentImgIndex = 0;
+            currentImgIndex++;//loop for the images. It says if the current
+          }else {  // image is less than the last image, to set it to the first
+            currentImgIndex = 0; //which is index 0
           }
           //show next image
-          $(".carousel-images")
-            .children()
-            .eq(currentImgIndex)
+          $(".carousel-images") // this tells it that once current img is
+            .children() //selected, to css style it to display in block form
+            .eq(currentImgIndex) //since it's originally set to none
             .css("display", "block");
 
         // for (let i = 0; i < data.length; i++) {
-          const randomNum = Math.floor(Math.random()* data.length)
+          const randomNum = Math.floor(Math.random()* data.length)//this selects the data(text)randomly to display on the page.
           // const $getDataFact = $('<div>')//div holding value from data
-          const $text = $('<p>').html(data[randomNum].text).addClass('fact') //div going into the $getDataFact
+          const $text = $('<p>').html(data[randomNum].text).addClass('fact') //this executes the above random fact to siplay the text and the addClass function overites or alternates with the above function that is set to remove fact, so that once removed it pushes the new fact.
         //
           // $text.html(data[i].text)
           // $text.html(data[1].text)
-          $data.append($text) //append to carousel
+          $data.append($text).css("font-size", "20px", "text-align","center") //appends data from API as text the the data functon.
           // $('body').append($data)//carousel to body
 
         // }//closing loop
-        $('body').append($data)
+        $('body').append($data)//appends data to the body.
         console.log(data)
 
       },//closing data function
