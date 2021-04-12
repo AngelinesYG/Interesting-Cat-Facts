5bbf74dd009b8554b2baa1653717e8825f2a69c1
Angelines Yaport-Garcia <angelinesyaport-garcia@Angeliness-MBP.fios-router.home>
2021-04-12 10:59:08 -0400

    Cat Facts draft3

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
 $(() => { //on load begins
 
+  let currentImgIndex = 0;
+  let lastIndex = $('.carousel-images').children().length-1;//+data
+  // let $userInput = $('input[type="click"]').val()
+  // let $getDataFact = $(event.target).val()
   $('button').on('click', (event)=> {
     // event.target.preventDefault()
-    let $userInput = $('input[type="click"]').val()
-    let $getDataFact = $(event.target).val()
 
     $.ajax({
-      url:'https://cat-fact.herokuapp.com/facts'
+      url:'https://cat-fact.herokuapp.com/facts',
 
-    }).then(
+    }).then( //make carousel here and append to it
       (data)=>{
-        for (let i = 0; i < data.length; i++) {
-          const $getDataFact = $('<div>')
-          const $text = $('<div>')
+        $('.fact').remove()
+        $(".carousel-images")
+        .children()
+        .eq(currentImgIndex)
+        .css("display","none");
         //
+        let $userInput = $('input[type="click"]').val()
+        // let $data = $(event.target).val('<div>')
+        const $data = $('<div>')
+
+          if (currentImgIndex < lastIndex) {
+            currentImgIndex++;
+            }else {
+            currentImgIndex = 0;
+          }
+          //show next image
+          $(".carousel-images")
+            .children()
+            .eq(currentImgIndex)
+            .css("display", "block");
+
+        // for (let i = 0; i < data.length; i++) {
+          const randomNum = Math.floor(Math.random()* data.length)
+          // const $getDataFact = $('<div>')//div holding value from data
+          const $text = $('<p>').html(data[randomNum].text).addClass('fact') //div going into the $getDataFact
         //
-        //   $text.html(data[i].length)
-          $getDataFact.append(data[i].text)
-          $('body').append($getDataFact)
-       }//closing loop
+          // $text.html(data[i].text)
+          // $text.html(data[1].text)
+          $data.append($text) //append to carousel
+          // $('body').append($data)//carousel to body
+
+        // }//closing loop
+        $('body').append($data)
         console.log(data)
 
       },//closing data function
